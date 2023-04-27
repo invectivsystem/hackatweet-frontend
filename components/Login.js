6ad71d6d698d9d73d998adeb9ebe1724759b2094
Remi <remigallois@gmail.com>
2023-04-27 12:02:09 +0200

    modifs

---
+++ components/Login.js
@@ -0,0 +1,18 @@
+import styles from '../styles/Login.module.css';
+
+function Login() {
+  return (
+    <div>
+      <main className={styles.main}>
+        <div className={styles.imgContainer}>
+
+        </div>
+        <div className={styles.loginContainer}>
+
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Login;
