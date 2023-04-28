2e43ded2a68904011e0e70b34ca1283c443c5bde
Remi <remigallois@gmail.com>
2023-04-28 12:14:49 +0200

    front

--- components/PageAccueil.js
+++ components/PageAccueil.js
@@ -1,14 +1,69 @@
 import React from 'react'
 import Tweet from './Tweet'
-import LastTweet from './LastTweet'
+import  LastTweet  from './LastTweet'
 import Trends from './Trends'
 
-export default function PageAccueil() {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { logout } from '../reducers/user'
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useRouter } from 'next/router'
+import styles from '../styles/PageAccueil.module.css'
+
+ function PageAccueil() {
+
+  const dispatch = useDispatch();
+  const router = useRouter();
+
+  const [signInUsername, setSignInUsername] = useState('');
+	const [signInPassword, setSignInPassword] = useState('');
+
+  const handleLogout = () => {
+		dispatch(logout());
+    router.push('/')
+	};
+
+  fetch('http://localhost:3000/users/signin', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ username: signInUsername, password: signInPassword }),
+		}).then(response => response.json())
+			.then(data => {
+				if (data.result) {
+					dispatch(login({ username: signInUsername, token: data.token }));
+					setSignInUsername('');
+					setSignInPassword('');
+				}
+			});
+
   return (
-    <div>
-      <Tweet></Tweet>
-      <Trends></Trends>
-      <LastTweet></LastTweet>
+    <div className={styles.content}>
+        <div className={styles.container1}>
+
+          <FontAwesomeIcon icon={faTwitter} className={styles.icon2} rotation={180}  /> 
+
+
+          
+
+              <button className={styles.logout} onClick={() => {handleLogout()}}>Logout</button>
+          
+        </div>
+        <div className={styles.container2}>
+          <h4 className={styles.home}>Home</h4>
+          <Tweet></Tweet>
+          <LastTweet></LastTweet>
+        </div>
+        <div className={styles.container3}>
+          <h4 className={styles.trend}>Trends</h4>
+
+           <Trends></Trends>
+
+        </div>
+        
+      
     </div>
   )
 }
+
+export default PageAccueil;
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,15 @@
 import "../styles/globals.css";
 import Head from "next/head";
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import user from "../reducers/user";
 
-/* import { persistStore, persistReducer } from "redux-persist";
-import { PersistGate } from "redux-persist/integration/react";
-import storage from "redux-persist/lib/storage";
 
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
- */
-/* const reducers = combineReducers({ reducername });
+ 
+ const reducers = combineReducers({ user });
 
 const persistConfig = { key: "hackatweet", storage };
 
@@ -17,18 +19,18 @@ const store = configureStore({
     getDefaultMiddleware({ serializableCheck: false }),
 });
 
-const persistor = persistStore(store); */
+const persistor = persistStore(store); 
 
 function App({ Component, pageProps }) {
   return (
-    <>
-       
+    <Provider store={store}>
+       <PersistGate persistor={persistor}>
         <Head>
           <title>Next.js App</title>
         </Head>
         <Component {...pageProps} />
-      
-</>
+      </PersistGate>
+    </Provider>
    );
 }
 
